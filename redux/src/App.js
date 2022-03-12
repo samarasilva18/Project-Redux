32db43db658d9d4ebb971ad16ce2dde4dba9ac41
samarasilva18 <samarasilvacandy@gmail.com>
2022-03-12 15:41:05 +0000

    Aula do dia 12/03

---
+++ redux/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css';
+import Card from './components/Card'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Projeto-Redux</h1>
+      <div className='linha'>
+        <Card> </Card>
+      </div>
+      <div className='linha'>
+        <Card></Card>
+      </div>
+    </div>
+  );
+}
+
+export default App;
